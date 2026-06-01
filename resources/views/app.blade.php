0de266ef4e7c3ed5300bb16c24f9db5266a2ac51
capsule214 <whitesibo@gmail.com>
2026-06-01 23:17:26 +0900

    修正：Uncaught Error: @vitejs/plugin-react can't detect preamble. Something is wrong.     at ContextMenu.jsx:9:9

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>製造スケジュール管理</title>
+    @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
 <body class="m-0 p-0 overflow-hidden" style="height:100vh;width:100vw;">
